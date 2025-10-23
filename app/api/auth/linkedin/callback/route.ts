070c8214c561d46d431237ed57774807c01e67b5
antoraphael2024 <antoraphael.elango@solutionchamps.com>
2025-10-23 13:04:59 +0530

    linked in oauth

---
+++ app/api/auth/linkedin/callback/route.ts
@@ -0,0 +1,58 @@
+import { errorResponse } from "@/app/api/_utility/oauthErrRes";
+import { NextResponse } from "next/server";
+
+export async function GET(req: Request): Promise<NextResponse> {
+  try {
+    const { searchParams } = new URL(req.url);
+    const code = searchParams.get("code");
+
+    if (!code) {
+      return errorResponse("ln_err", "Failed to log in with LinkedIn");
+    }
+
+    let url = `${process.env.NEXT_PUBLIC_API_URL_IDN || "https://identity-api.sandbox.pepagora.org/"}auth/oauthVerification`;
+
+    const nestRes = await fetch(url, {
+      method: "PATCH",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        code: code,
+        type: "linkedIn",
+      }),
+    });
+    const nestResJSON = await nestRes.json();
+
+    if (!nestResJSON || nestResJSON?.statusCode !== 200) {
+      return errorResponse("ln_err", "Failed to log in with LinkedIn");
+    }
+    const date = new Date();
+    date.setTime(date.getTime() + 1 * 24 * 60 * 60 * 1000);
+    let expires = `; expires=${date.toUTCString()}`;
+    const response = new NextResponse(null, { status: 307 });
+    if (nestResJSON?.data?.onboardSession) {
+
+      response.headers.set(
+        "Set-Cookie",
+        `onboardSession=${nestResJSON.data.onboardSession}; Path=/; Max-Age=${expires}`
+      );
+    } else if (nestResJSON?.data?.userSession) {
+      response.headers.set(
+        "Set-Cookie",
+        `userSession=${nestResJSON.data.userSession}; Path=/; Max-Age=${expires}`
+      );
+    } else if (nestResJSON?.data?.clientSessionId) {
+      response.headers.set(
+        "Set-Cookie",
+        `authSession=${nestResJSON.data.clientSessionId}; Path=/; Max-Age=${expires}`
+      );
+    } else {
+      console.error("OAuth error");
+      return errorResponse("ggl_err", "Failed to log in with Google");
+    }
+    response.headers.set("Location", "/authenticate");
+    return response;
+  } catch (error) {
+    console.error("LinkedIn OAuth error:", error);
+    return errorResponse("ln_err", "Failed to log in with LinkedIn");
+  }
+}
